0c1bf640f074f1a5d9aa10aede83fa057e4ca1bb
Tyler Lawson <tylerlawson12@live.com>
2019-11-05 09:29:25 -0800

    added code to server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// npm packages - 3rd party
+require(‘dotenv’).config()
+const express = require(‘express’);
+const cors = require(‘cors’);
+// application constant
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get(‘/cool’, (request, response) => {
+ response.send(‘cool data from the / cool route’);
+});
+
+
+
+
+
+
+
